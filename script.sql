e57101928052e5e72a3c8b3a2c4898b89e92c23b
Lili Elaine Eagles <leeagles@users.noreply.github.com>
2018-03-09 13:56:16 +0000

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,4 @@
+create table employee(id int(10) not null primary key auto_increment, firstname varchar(20), lastname varchar(20));
+
+insert into employee(firstname,lastname) values('Eagles','Elaine');
+insert into employee(firstname,lastname) values('Eagles','Ryan'); 
